3e16801b95f29dbe2f77ddb13082d165b9374d5b
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-04 16:51:45 -0300

    Requisito 1, Teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    returnAssert = [
+        "O produto CADEIRA fabricado em 2022-04-04 por Forces of Nature "
+        "com validade até 2023-02-09"
+        "precisa ser armazenado Conservar em local fresco."
+        ]
+    assert Product(
+        '1', 'CADEIRA', 'Forces of Nature', '2022-04-04', '2023-02-09', "FR48",
+        'Conservar em local fresco'
+        ) == returnAssert
