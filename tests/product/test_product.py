42e405f6d874e00247d5624a1f6a38c87e6a7029
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-04 17:14:45 -0300

    Requisito 1, passando

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,12 +2,17 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    returnAssert = [
-        "O produto CADEIRA fabricado em 2022-04-04 por Forces of Nature "
-        "com validade até 2023-02-09"
-        "precisa ser armazenado Conservar em local fresco."
-        ]
-    assert Product(
-        '1', 'CADEIRA', 'Forces of Nature', '2022-04-04', '2023-02-09', "FR48",
+    product_Mock = Product(
+        1, 'CADEIRA', 'Forces of Nature', '2022-04-04', '2023-02-09', 'FR48',
         'Conservar em local fresco'
-        ) == returnAssert
+    )
+
+    return_Assert = (
+        "O produto CADEIRA"
+        " fabricado em 2022-04-04"
+        " por Forces of Nature com validade"
+        " até 2023-02-09"
+        " precisa ser armazenado Conservar em local fresco."
+    )
+
+    assert product_Mock.__repr__() == return_Assert
