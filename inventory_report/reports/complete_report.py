e7d3a729cd99adc0589a9ee9ee59245dc79b05a3
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-05 15:47:51 -0300

    Requisito 3, complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,29 @@
+from inventory_report.reports.simple_report import SimpleReport
+import numpy as np
+
+
 class CompleteReport:
-    pass
+
+    @classmethod
+    def company_with_more_products(cls, list):
+        lista = []
+        stock = ""
+        for company in list:
+            lista.append(company["nome_da_empresa"])
+
+        fdist = dict(zip(*np.unique(lista, return_counts=True)))
+
+        for key, value in fdist.items():
+            stock += f"- {key}: {value}\n"
+
+        return stock
+
+    @classmethod
+    def generate(cls, list):
+        simple_rep = SimpleReport.generate(list)
+        complete_rep = cls.company_with_more_products(list)
+        return (
+            f"{simple_rep}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{complete_rep}"
+        )
