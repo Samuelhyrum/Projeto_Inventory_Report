33b08fdcdc53c0461a47d2ca616c44f3f0990d0b
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-06 11:03:24 -0300

    Requisito 5, inventory json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,16 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
+
+    File_path = {
+        "inventory_report/data/inventory.csv": "csv_import_data",
+        "inventory_report/data/inventory.json": "json_import_data",
+
+    }
 
     REPORTS = {
         "simples": SimpleReport.generate,
@@ -12,8 +19,15 @@ class Inventory:
 
     @classmethod
     def import_data(cls, path, type):
-        with open(path, encoding="utf8") as file:
-            all_file = csv.DictReader(file, delimiter=",", quotechar='"')
-            lista = list(all_file)
+        lista = getattr(cls, cls.File_path[path])(path)
+        return cls.REPORTS[type](lista)
 
-            return cls.REPORTS[type](lista)
+    @classmethod
+    def csv_import_data(cls, path):
+        with open(path, encoding="utf8") as file:
+            return list(csv.DictReader(file, delimiter=",", quotechar='"'))
+
+    @classmethod
+    def json_import_data(cls, path):
+        with open(path, encoding="utf8") as file:
+            return json.load(file)
